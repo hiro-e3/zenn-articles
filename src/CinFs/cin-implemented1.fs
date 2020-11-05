b07197d341a940f12a25c04cb87b8de033208106
Iida Hiroki <minoru113@hotmail.com>
2020-11-05 20:29:03 +0900

    srcディレクトリの構成変更、プロジェクトファイル追加

--- src/CinFs/cin-implemented1.fs
+++ src/CinFs/cin-implemented1.fs
@@ -1,4 +1,24 @@
 namespace Program
+    module CinQ = 
+        open System
+        open System.Collections.Generic
+        open System.ComponentModel
+
+        type CIn = 
+            | ReadStream of readStream : (unit -> Queue<string>)
+            | Tokens of queue : Queue<string>
+
+        let cin = 
+            ReadStream (fun () -> new Queue<string>(stdin.ReadLine().Split(' ')))
+
+        let inline (>>) (cin : CIn) (x : byref<'U>) =
+            let q = match cin with
+                    | ReadStream f -> f ()
+                    | Tokens t -> t
+            let converter = TypeDescriptor.GetConverter(typeof<'U>)
+            x <- converter.ConvertFromString(q.Dequeue()) :?> 'U
+            Tokens q
+
     module IOStream =
         open System
         open System.IO
